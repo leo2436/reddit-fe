eaf4170068060b20e35ea1b853a535ddf1b6e274
leo2436 <chenll1996@foxmail.com>
2020-09-28 23:29:03 +0800

    pagination

--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -40,6 +40,7 @@ export type Post = {
   creatorId: Scalars['Float'];
   createdAt: Scalars['String'];
   updatedAt: Scalars['String'];
+  textSnippet: Scalars['String'];
 };
 
 export type User = {
@@ -238,7 +239,7 @@ export type PostsQuery = (
   { __typename?: 'Query' }
   & { posts: Array<(
     { __typename?: 'Post' }
-    & Pick<Post, 'id' | 'title' | 'createdAt' | 'updatedAt'>
+    & Pick<Post, 'id' | 'title' | 'createdAt' | 'updatedAt' | 'textSnippet'>
   )> }
 );
 
@@ -351,6 +352,7 @@ export const PostsDocument = gql`
     title
     createdAt
     updatedAt
+    textSnippet
   }
 }
     `;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,69 @@
-import { Link } from "@chakra-ui/core";
+import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/core";
 import { withUrqlClient } from "next-urql";
 import { Layout } from "../components/Layout";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import NextLink from "next/link";
+import React, { useState } from "react";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
-    variables: {
-      limit: 10,
-    },
+  const [variables, setVariables] = useState({
+    limit: 10,
+    cursor: null as null | string,
   });
+  const [{ data, fetching }] = usePostsQuery({
+    variables,
+  });
+
+  if (!fetching && !data) {
+    return <div>you got query failed for some reason</div>;
+  }
+
   return (
-    <Layout variant="small">
-      <NextLink href="create-post">
-        <Link>Create a Post</Link>
-      </NextLink>
+    <Layout variant="regular">
+      <Flex alignItems="center">
+        <Heading>LiReddit</Heading>
+        <NextLink href="create-post">
+          <Link ml="auto">Create a Post</Link>
+        </NextLink>
+      </Flex>
       <br />
-      {!data ? (
+      {!data && fetching ? (
         <div>loading....</div>
       ) : (
-        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+        <Stack spacing={8}>
+          {data.posts.map((p) => (
+            <Box
+              key={p.id}
+              p={5}
+              shadow="md"
+              borderWidth="1px"
+              flex="1"
+              rounded="md"
+            >
+              <Heading fontSize="xl">{p.title}</Heading>
+              <Text mt={4}>{p.textSnippet}</Text>
+            </Box>
+          ))}
+        </Stack>
       )}
+      {data ? (
+        <Flex>
+          <Button
+            onClick={() => {
+              setVariables({
+                limit: 10,
+                cursor: data.posts[data.posts.length - 1].createdAt,
+              });
+            }}
+            isLoading={fetching}
+            m="auto"
+            my={8}
+          >
+            Load More
+          </Button>
+        </Flex>
+      ) : null}
     </Layout>
   );
 };
